68721ecd065d2ed641d2ce302db97b1b772a3c42
sudhanshujaiswal <jaiswalsudhanshu20@gmail.com>
2024-01-30 20:13:35 +0530

    Initial Commit

---
+++ zomato/src/main/java/com/zomato/ZomatoApplication.java
@@ -0,0 +1,13 @@
+package com.zomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoApplication.class, args);
+	}
+
+}
---
+++ zomato/src/test/java/com/zomato/ZomatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zomato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
